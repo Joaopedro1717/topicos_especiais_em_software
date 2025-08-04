82896c4cc999db122c883fbe457552869d5a05ec
Joaopedro1717 <j.pedrominuto@hotmail.com>
2025-08-04 20:45:18 -0300

    ajuste exercicio09

--- LISTA01/exercicio02.py
+++ LISTA01/exercicio02.py
@@ -2,4 +2,6 @@
 """Conversão de temperatura de Celsius para Fahrenheit"""
 grausCelsius = float(input('Digite a temperatura em graus Celsius: '))
 grausFahrenheit = (grausCelsius * 9/5) + 32
+grausKelvin = grausCelsius + 273.15
 print(f'A temperatura em graus Fahrenheit é: {grausFahrenheit}°F')
+print(f'A temperatura em graus Kelvin é: {grausKelvin}K')
--- LISTA01/exercicio09.py
+++ LISTA01/exercicio09.py
@@ -1,25 +1,9 @@
 """palindromo"""
 
-import unicodedata
-
-def limpar_texto(texto):
-    # Remove acentos, transforma em minúsculas e remove espaços e pontuações
-    texto = texto.lower()
-    texto = ''.join(
-        c for c in unicodedata.normalize('NFD', texto)
-        if unicodedata.category(c) != 'Mn'  # remove acentos
-    )
-    texto = ''.join(c for c in texto if c.isalnum())  # remove espaços e pontuação
-    return texto
-
-# Entrada
 entrada = input("Digite uma palavra ou frase: ")
 
-# Limpeza do texto
-texto_limpo = limpar_texto(entrada)
-
-# Verificação de palíndromo
-if texto_limpo == texto_limpo[::-1]:
+# Verificação de palíndromo, utiliza o [::-1] que inverte a string, faz uma comparação com o texto original, caso for igual, é um palíndromo
+if entrada == entrada[::-1]:
     print("É um palíndromo!")
 else:
     print("Não é um palíndromo.")
